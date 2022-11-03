d653da60c2ec83db83d7ab9e13efc50d63a8574d
Francisco <francisco.gm97@hotmail.com>
2022-11-02 23:01:27 -0300

    initialize backend

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,8 @@
 export const Form = () => {
   return (
-    <form>
-      <input type="email" placeholder="E-mail" />
-      <input type="password" placeholder="password" />
+    <form method="post" action="/backend/register.php">
+      <input type="email" placeholder="E-mail" name="email"/>
+      <input type="password" placeholder="password" name="pass"/>
       <button type="submit">Send</button>
     </form>
   );
